2783d0ed01c3aabdf94028dd2c0723850739e666
ramazan <coolstoves@gmail.com>
2025-08-21 23:57:13 +0300

    2108252357

---
+++ src/TEKRAR/KullanıcıdanDataAlma/D05_CemberinCevresiniYazdirma.java
@@ -0,0 +1,27 @@
+package TEKRAR.KullanıcıdanDataAlma;
+
+import java.util.Scanner;
+
+public class D05_CemberinCevresiniYazdirma {
+    public static void main(String[] args) {
+
+        //Soru 6 Kullanıcıdan bir cemberin yarıcapını alıp,
+        //cevresini ve alanını yazdırın...
+
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("lutfen demberin yarıcapını cm olarak girimiz...");
+        double yariCap=scanner.nextDouble();
+        double Pi=3.14;
+
+        System.out.println("Çemberin cevresi : "+(2*Pi*yariCap));
+        System.out.println("Cemberin alanı : "+(Pi*yariCap*yariCap));
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/TEKRAR/KullanıcıdanDataAlma/D06_Swap.java
@@ -0,0 +1,30 @@
+package TEKRAR.KullanıcıdanDataAlma;
+
+import java.util.Scanner;
+
+public class D06_Swap {
+    public static void main(String[] args) {
+
+        //Soru 7 -Kullaıcıdan iki sayı alıp
+        //ikisinin degerini degistirin
+
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("lütfen ilk sayıyı giriniz");
+        int IlkSayi=scanner.nextInt();
+        System.out.println("lütfen ikinci sayıyı giriniz");
+        int IkinciSayi=scanner.nextInt();
+
+
+        int degisimci=0;
+
+        degisimci=IlkSayi;
+        IlkSayi=IkinciSayi;
+        IkinciSayi=degisimci;
+
+        System.out.println("ilk sayinin yenidegeri : "+IlkSayi+"\nikinciSayinin yeni degeri:  "+IkinciSayi);
+
+
+
+
+    }
+}
