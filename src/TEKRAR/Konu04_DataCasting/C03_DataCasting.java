e3167c36d5e760678b4d1cd90b42eacc08ff9d78
ramazan <coolstoves@gmail.com>
2025-08-22 23:12:30 +0300

    2202252312

--- src/TEKRAR/Konu04_DataCasting/C03_DataCasting.java
+++ src/TEKRAR/Konu04_DataCasting/C03_DataCasting.java
@@ -1,9 +1,22 @@
 package TEKRAR.Konu04_DataCasting;
 
+import java.util.Scanner;
+
 public class C03_DataCasting {
     public static void main(String[] args) {
 
+//kullanicidan iki tamsayi alin
+//sayilari birbirine bölüp, işlem sonucunu ondalıklı olarak yazdırn
 
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("lütfen iki tamsayı giriniz");
+        int sayi1=scanner.nextInt();
+        int sayi2=scanner.nextInt();
+
+        double sayi3;
+
+        sayi3=(double)sayi1/sayi2;
+        System.out.println("Verdiğiniz sayılıların bolum sonucu : "+sayi3);
 
 
 
