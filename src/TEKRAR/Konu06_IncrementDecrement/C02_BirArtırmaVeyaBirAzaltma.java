d3af8a848d23e357cac864e6d03d9412e441521c
ramazan <coolstoves@gmail.com>
2025-08-27 23:34:47 +0300

    2708252334

---
+++ src/TEKRAR/Konu06_IncrementDecrement/C02_BirArtırmaVeyaBirAzaltma.java
@@ -0,0 +1,27 @@
+package TEKRAR.Konu06_IncrementDecrement;
+
+public class C02_BirArtırmaVeyaBirAzaltma {
+    public static void main(String[] args) {
+
+        int a= 20;
+        int b=40;
+
+        ++a;
+
+        System.out.println(a);
+
+        a++;
+
+        System.out.println(a);
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/TEKRAR/Konu06_IncrementDecrement/C03_PreIncrement_PostIncrement.java
@@ -0,0 +1,27 @@
+package TEKRAR.Konu06_IncrementDecrement;
+
+public class C03_PreIncrement_PostIncrement {
+    public static void main(String[] args) {
+        int a=20;
+
+        //a nın degerini olusturdugunuz bir b variablesine atayın
+        //sonra a yi bir atayın;
+
+        int b=a++;
+
+        System.out.println("10. satirda a: " +a+ ", b : "+ b);//a=21,b=20
+
+
+        int c=20;
+
+        //once c yi 1 artırın
+        //sonra c nin degerini olusturdugumuz bir d variablesine atayın
+
+        int d=++c;
+        System.out.println("10. satirda c: " +c+ ", d : "+ d);//c=21,d=21
+
+
+
+
+    }
+}
