12e48435b68d5c5a752dca9a8a74d9c8c9d4b75a
ramazan <coolstoves@gmail.com>
2025-09-04 23:09:28 +0300

    040925

--- src/TEKRAR/Konu07_RelationalOperators/C02_AndOperatoru.java
+++ src/TEKRAR/Konu07_RelationalOperators/C02_AndOperatoru.java
@@ -25,7 +25,30 @@ public class C02_AndOperatoru {
         //eger o islemler yapılmazsa atamalarda yapılmamıs olur
         //bu durumda sonuc false olsa da tum islemleri yapılmasını isteriz...
 
+        boolean sonuc1=true;
+        boolean sonuc2=true;
+        boolean sonuc3=true;
 
+        boolean genelSonuc=(sonuc1= a>=b) & (sonuc2 = a<b) &(sonuc3=a>b-8);
+
+        System.out.println(genelSonuc);//false
+        System.out.println(sonuc1);//false
+        System.out.println(sonuc2);//true
+        System.out.println(sonuc3);//false
+
+        //int a=10;
+       // int b=20;
+
+        boolean sonuc4=true;
+        boolean sonuc5=true;
+        boolean sonuc6=true;
+
+        boolean genelSonuc3=(sonuc4= a>=b) && (sonuc5 = a<b) && (sonuc6=a>b-8);
+
+        System.out.println(genelSonuc3);//false
+        System.out.println(sonuc4);//false
+        System.out.println(sonuc5);//true HIC BAKMADI ATAMA İŞLEMLERINDE KULLANILMAMALI...
+        System.out.println(sonuc6);//true
 
 
 
