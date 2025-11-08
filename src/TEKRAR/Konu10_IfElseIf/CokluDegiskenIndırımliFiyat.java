bea087a31746e6adaa0cacf9b96081bd7c578a2d
ramazan <coolstoves@gmail.com>
2025-11-08 15:15:09 +0300

    081120251555

--- src/TEKRAR/Konu10_IfElseIf/CokluDegiskenIndırımliFiyat.java
+++ src/TEKRAR/Konu10_IfElseIf/CokluDegiskenIndırımliFiyat.java
@@ -18,16 +18,24 @@ public class CokluDegiskenIndırımliFiyat {
         int urunAdedi = scanner.nextInt();
         System.out.println("lütfen urun fiyatı giriniz....");
         double fiyat=scanner.nextDouble();
+        System.out.println("MUSTERI KARTINIZ VARSA V YOKSA Y yazınız");
+        char kartDurumu=scanner.next().toUpperCase().charAt(0);
 
+        if(kartDurumu=='V'&& urunAdedi>=10){
 
+            System.out.println("%20 indirim ile ödeyeceğiniz tutar :"+ urunAdedi*fiyat*(80/100));
 
+        } else if (kartDurumu=='V'&& urunAdedi<10) {
 
+            System.out.println("%15 indirim ile ödeyeceğiniz tutar :"+ urunAdedi*fiyat*(85/100));
 
+        } else if (kartDurumu=='Y'&& urunAdedi>=10) {
 
+            System.out.println("%15 indirim ile ödeyeceğiniz tutar :"+ urunAdedi*fiyat*(85/100));
+        }else if (kartDurumu=='Y'&& urunAdedi>=10) {
 
-
-
-
+            System.out.println("%10 indirim ile ödeyeceğiniz tutar :"+ urunAdedi*fiyat*(90/100));
+        }
 
 
     }
