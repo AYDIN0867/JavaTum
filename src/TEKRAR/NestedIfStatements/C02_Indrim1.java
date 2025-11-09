c407242a173a1b83ee9cb20c8a97b7b5e4aea0ba
ramazan <coolstoves@gmail.com>
2025-11-09 16:04:12 +0300

    091120251604

--- src/TEKRAR/NestedIfStatements/C02_Indrim1.java
+++ src/TEKRAR/NestedIfStatements/C02_Indrim1.java
@@ -25,29 +25,73 @@ public class C02_Indrim1 {
 
         if (kartDurumu=='V'){
 
-          if (urunAdedi>=10){
+            if (urunAdedi<=0){
 
-              System.out.println("odemeniz gereken ücret %20 indirim hesaplanarak" + (urunAdedi*fiyat)*80/100 );
+                System.out.println( "urun adedi sıfır ve sıfırdan kucuk olamaz ");
+            }  else if (urunAdedi>=10) {
 
-          } else if (urunAdedi<10) {
+                System.out.println("odemeniz gereken ücret %20 indirim hesaplanarak" + (urunAdedi * fiyat) * 80 / 100);
 
-              System.out.println("odemeniz gereken ücret %15 indirim hesaplanarak" + (urunAdedi*fiyat)*85/100 );
-          }
+            }else  {
+
+                System.out.println("odemeniz gereken ücret %15 indirim hesaplanarak" + (urunAdedi * fiyat) * 85 / 100);
+
+            }
 
 
         } else if (kartDurumu=='Y') {
 
-            if (urunAdedi>=10){
+            if (urunAdedi<=0){
 
-                System.out.println("odemeniz gereken ücret %15 indirim hesaplanarak" + (urunAdedi*fiyat)*85/100 );
+                System.out.println( "urun adedi sıfır ve sıfırdan kucuk olamaz ");
+            }  else if (urunAdedi>=10) {
 
-            } else if (urunAdedi<10) {
+                System.out.println("odemeniz gereken ücret %15 indirim hesaplanarak" + (urunAdedi * fiyat) * 85 / 100);
 
-                System.out.println("odemeniz gereken ücret %15 indirim hesaplanarak" + (urunAdedi*fiyat)*90/100 );
+            }else  {
+
+                System.out.println("odemeniz gereken ücret %10 indirim hesaplanarak" + (urunAdedi * fiyat) * 90 / 100);
+
             }
 
 
         }else System.out.println("KART DURUMU ICIN V yada Y yazmalısınız");
+
+
+        //ana değiken ürün adedi olsun
+
+        if (urunAdedi<0){
+
+            System.out.println("ürün adedi sifirdan az olmaz ");
+
+
+
+        } else if (urunAdedi>=10) {
+
+            if (kartDurumu=='V'){
+
+                System.out.println("ödenmesi gereken %20 indirim ile :" + urunAdedi*fiyat*80/100);
+
+            } else if (kartDurumu=='Y') {
+
+                System.out.println("ödenmesi gereken %20 indirim ile :" + urunAdedi*fiyat*85/100);
+
+            }
+
+
+        }else {//ürün adedi 0-9 arasi olanlar
+
+            if (kartDurumu=='V'){
+
+                System.out.println("ödenmesi gereken %15 indirim ile :" + urunAdedi*fiyat*85/100);
+
+            } else if (kartDurumu=='Y') {
+
+                System.out.println("ödenmesi gereken %20 indirim ile :" + urunAdedi*fiyat*90/100);
+            }
+
+
+        }
 
 
     }
