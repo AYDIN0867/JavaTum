ff0b954d3892c7e91872cb45b44a1b7729ad9fab
ramazan <coolstoves@gmail.com>
2025-11-13 19:46:52 +0300

    13112025

---
+++ src/TEKRAR/Konu14_SubString/C02_Substring_IkiParametre.java
@@ -0,0 +1,17 @@
+package TEKRAR.Konu14_SubString;
+
+public class C02_Substring_IkiParametre {
+    public static void main(String[] args) {
+
+
+        String str="giden gitmiştir gittiği gün bitmiştir";
+
+        //ilk kelime hariç yazdırın...
+
+        System.out.println(str.substring(5));
+
+
+
+
+    }
+}
