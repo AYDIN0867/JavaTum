c294347ecfad0a61f1bc070f78acc1cad4eab60e
ramazan <coolstoves@gmail.com>
2025-11-16 12:06:03 +0300

    16112025

---
+++ src/TEKRAR/Konu15_StartsWith/C02_EndsWith.java
@@ -0,0 +1,31 @@
+package TEKRAR.Konu15_StartsWith;
+
+public class C02_EndsWith {
+    public static void main(String[] args) {
+
+String str = "hayat güzel durmadan yürürsen";
+
+//str "yürürsen" ile mi bitiyor
+
+        System.out.println(str.endsWith("yürürsen"));//true
+
+//   metin "yürürsen" içeriyor mu
+
+        System.out.println(str.contains("yürürsen"));//true
+
+// str "yürürsen" ile mi başlıyor
+
+        System.out.println(str.startsWith("yürürsen"));//false
+
+// str "hayat" mı?
+
+        System.out.println(str.equals("hayat"));//false
+
+// str "sen" ile mi bitiyor
+
+        System.out.println(str.endsWith("sen"));//true
+
+
+
+    }
+}
