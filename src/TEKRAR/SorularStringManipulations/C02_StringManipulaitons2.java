e022c7485928862c3af5818a74192cb6d50893d7
ramazan <coolstoves@gmail.com>
2025-11-19 21:31:42 +0300

    19112025

---
+++ src/TEKRAR/SorularStringManipulations/C02_StringManipulaitons2.java
@@ -0,0 +1,20 @@
+package TEKRAR.SorularStringManipulations;
+
+public class C02_StringManipulaitons2 {
+    public static void main(String[] args) {
+
+
+        // Kullanicidan belirli bir formatta verdiği String fiyatlari toplayip yazdirin...
+
+        // input1 : "15.30 €", input2: "11.45 € "
+        // output : 26.75 €
+        // Not: sondaki para birimi degisken olamalidir...
+
+
+
+        String fiyat1= "15.30 €";
+        String fiyat2= "11.45 €";
+
+
+    }
+}
