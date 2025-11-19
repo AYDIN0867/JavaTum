0b9856b1e8dd4ae10cc0f435533c184974a56f20
ramazan <coolstoves@gmail.com>
2025-11-19 13:28:20 +0300

    19112025

---
+++ src/TEKRAR/Konu16_Empty_Length/C05_VerilenMetniTemizleme.java
@@ -0,0 +1,35 @@
+package TEKRAR.Konu16_Empty_Length;
+
+import java.util.Scanner;
+
+public class C05_VerilenMetniTemizleme {
+    public static void main(String[] args) {
+
+        //kullanıcıdan bir metin isteyin
+        // metinde bosluk ve harf olamayan herseyi silin
+        // ornek : input :J1a4v*a )G*&^56uzel_!dir.
+        // output : Java Guzeldir
+
+
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("lütfen bir cümle giriniz...");
+
+        String metin=scanner.nextLine();
+
+        String metin1=metin.replaceAll("\\d","");
+        //suan metin1 de rakam olmadıgını biliyoruz boşlukları korumak için boşlukların yerine bir şey yazıp sonra silelim
+        metin1=metin1.replaceAll(" ","1");
+
+        metin1=metin1.replaceAll("\\W", "");
+
+        //_ çizgi iin özel yazmamız lazım
+
+        metin1=metin1.replaceAll("_", "");
+
+        metin1=metin1.replaceAll("1"," ");
+        metin1+=".";
+        System.out.println(metin1);
+
+
+    }
+}
---
+++ src/TEKRAR/Konu16_Empty_Length/C06_BilgileriDüzenleme.java
@@ -0,0 +1,50 @@
+package TEKRAR.Konu16_Empty_Length;
+
+import java.util.Scanner;
+
+public class C06_BilgileriDüzenleme {
+    public static void main(String[] args) {
+
+// Kullanıcıdan ismini, soyismini, ve 16 haneli kredi karti numarasını alin
+// bilgileri asagıdaki gibi yazdirin
+// girilen isim-soyisim  : C** D***,
+// Kredi kart numarasi : **** **** **** 4567
+
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("lütfen isminizi giriniz....");
+        String isim=scanner.nextLine();
+        System.out.println("lütfen soyisminizi giriniz....");
+        String soyIsim=scanner.nextLine();
+        System.out.println("lütfen kredi kart numarazanızı giriniz");
+        String kredikartıNo=scanner.nextLine();
+
+       isim= isim.substring(0,1).toUpperCase()+isim.substring(1).replaceAll("\\w","*");
+       soyIsim=soyIsim.substring(0,1).toUpperCase()+soyIsim.substring(1).replaceAll("\\w","*");
+       kredikartıNo=kredikartıNo.substring(0,4).replaceAll("\\d","*")
+               +" "
+               +kredikartıNo.substring(5,9).replaceAll("\\d","*")
+               +" "
+               +kredikartıNo.substring(10,14).replaceAll("\\d","*")
+               +" "
+               +kredikartıNo.substring(15);
+
+
+        System.out.println(
+
+             "girilen isim-soyisim :" + isim + " "+soyIsim + "\n Kredi kartı numarası :" + kredikartıNo
+
+
+        );
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
