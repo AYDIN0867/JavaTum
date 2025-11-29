92f5e7aa0d0877130d0290c06aa3448653b5e7c3
ramazan <coolstoves@gmail.com>
2025-11-29 12:43:30 +0300

    29112025

---
+++ src/TEKRAR/Konu20_MetodOlusturma/C03_MetodKullanımı.java
@@ -0,0 +1,13 @@
+package TEKRAR.Konu20_MetodOlusturma;
+
+public class C03_MetodKullanımı {
+    public static void main(String[] args) {
+
+        String str="devam et durmadan...";
+
+        System.out.println(str.toUpperCase());
+
+
+
+    }
+}
