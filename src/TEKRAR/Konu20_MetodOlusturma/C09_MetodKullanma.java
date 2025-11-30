0b53d901dcb105b886b0ec26271ce4a0b144f570
ramazan <coolstoves@gmail.com>
2025-11-30 12:17:19 +0300

    30112025

---
+++ src/TEKRAR/Konu20_MetodOlusturma/C09_MetodKullanma.java
@@ -0,0 +1,14 @@
+package TEKRAR.Konu20_MetodOlusturma;
+
+public class C09_MetodKullanma {
+    public static void main(String[] args) {
+
+
+        C06_MetniTerstenYazdırma.metniTerstenYaz("ramazan");
+        System.out.println(" ");
+        System.out.println(C07_IsmiDüzenle.isimDuzenle("ramazan", "saltık"));
+
+        System.out.println(C08_TerstenYazdir.tersYazılısınıDondur("merkez"));
+
+    }
+}
