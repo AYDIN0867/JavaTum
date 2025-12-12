e38a32598a7e2e513b1f2bc95c6278326aac20a9
ramazan <coolstoves@gmail.com>
2025-12-12 14:41:13 +0300

    12122025

--- src/TEKRAR/Konu23_DoWhileLoop/C06_KarekokBull.java
+++ src/TEKRAR/Konu23_DoWhileLoop/C06_KarekokBull.java
@@ -1,5 +1,7 @@
 package TEKRAR.Konu23_DoWhileLoop;
 
+import java.util.Scanner;
+
 public class C06_KarekokBull {
     public static void main(String[] args) {
 
@@ -7,6 +9,34 @@ public class C06_KarekokBull {
         //         sayının tam kare olup olmadığını bulunuz,
         //         tamkare ise true değilse false yazdırınız.
         //	Ornek :  input : 16 (4*4) , output: true
+
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Lutfen bir tamsayi giriniz....");
+        int girilenSayi=scanner.nextInt();
+
+
+        int karekokDegeri=1;
+
+        do {
+
+            if (karekokDegeri*karekokDegeri==girilenSayi) {
+                System.out.println("girilensayı tamkaredir..");
+                break;
+            }
+            karekokDegeri++;
+            if (karekokDegeri*karekokDegeri>girilenSayi){
+
+                System.out.println("tam kare değildir");
+                break;
+            }
+
+
+        }while (karekokDegeri*karekokDegeri<=girilenSayi);
+
+
+
+
+
 
 
     }
