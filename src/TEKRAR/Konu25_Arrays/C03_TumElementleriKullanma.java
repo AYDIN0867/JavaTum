ba3dae6aa5c9b273d586fcc86abad25cfa6ef157
ramazan <coolstoves@gmail.com>
2025-12-17 08:51:51 +0300

    17122025

---
+++ src/TEKRAR/Konu25_Arrays/C03_TumElementleriKullanma.java
@@ -0,0 +1,16 @@
+package TEKRAR.Konu25_Arrays;
+
+import java.util.Arrays;
+
+public class C03_TumElementleriKullanma {
+    public static void main(String[] args) {
+
+        int [] sayilar={4,7,1,3,4,8,0,1};
+
+        //sayilar array'ini yazdirin...
+
+        System.out.println(Arrays.toString(sayilar)); //[4, 7, 1, 3, 4, 8, 0, 1]
+
+
+    }
+}
