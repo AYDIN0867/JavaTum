6ecbeaf04f14c031b81e36f91e27a140d7996593
ramazan <coolstoves@gmail.com>
2025-12-17 15:03:33 +0300

    17122025

--- src/TEKRAR/Konu25_Arrays/C03_TumElementleriKullanma.java
+++ src/TEKRAR/Konu25_Arrays/C03_TumElementleriKullanma.java
@@ -1,5 +1,6 @@
 package TEKRAR.Konu25_Arrays;
 
+import javax.swing.*;
 import java.util.Arrays;
 
 public class C03_TumElementleriKullanma {
@@ -11,6 +12,56 @@ public class C03_TumElementleriKullanma {
 
         System.out.println(Arrays.toString(sayilar)); //[4, 7, 1, 3, 4, 8, 0, 1]
 
+        //sayilar array'inin tum elementlerini yan yana yazdirin...
+
+        for (int i = 0; i < sayilar.length; i++) {
+
+            System.out.print(sayilar[i]+" ");
+
+        }
+
+        /*
+
+        EGER arrayin tüm elementlerine ulaşmak gerekiyorsa
+        for loop kullnaırız...
+
+        */
+
+
+    // sayilar array'inin tum elementlerin toplamını yazdirin...
+
+        int toplam=0;
+
+        for (int i = 0; i < sayilar.length; i++) {
+
+            toplam+=sayilar[i];
+
+        }
+
+        System.out.println("sayılar array indeki elementlerin toplamı :"+ toplam);
+
+
+        //sayılara arry indeki çift sayıların carpımı
+
+        int carpım=1;
+
+        for (int i = 0; i < sayilar.length; i++) {
+
+            if (sayilar[i]%2==0){
+
+                carpım*=sayilar[i];
+
+            }
+
+
+        }
+
+        System.out.println("sayılar arrayindeki çift sayıların carpımı :"+ carpım);
 
     }
+
+
+
+
+
 }
---
+++ src/TEKRAR/Konu25_Arrays/C05_PozitifElementleriTopla.java
@@ -0,0 +1,48 @@
+package TEKRAR.Konu25_Arrays;
+
+public class C05_PozitifElementleriTopla {
+    public static void main(String[] args) {
+
+        //verilen bir array'deki pozitif tamsayıların toplamını yazdırın...
+
+        int[] arr = {-4, -6, 7, 2, 5, -1, 0, 4, -5, 3};
+        int toplam = 0;
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] > 0) {
+
+                toplam += arr[i];
+
+            }
+
+
+        }
+
+        System.out.println("arr array indeki pozitif elementlerin toplamı :" + toplam);
+
+        //verilen bir arraydeki pozitif tamsayıları toplayıp
+        //sonucunu bize donduren bir metod yazdırın...
+
+
+    }
+
+    public static int pozitifSayılarıTopla(int[] arr) {
+
+
+        int toplam = 0;
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] > 0) {
+
+                toplam += arr[i];
+
+            }
+
+
+        }
+
+        return toplam;
+    }
+
+
+}
