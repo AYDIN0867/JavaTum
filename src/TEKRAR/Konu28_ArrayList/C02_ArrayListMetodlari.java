f550d8d21602846f161658e09650d68b52849966
ramazan <coolstoves@gmail.com>
2025-12-22 20:56:53 +0300

    22122025

---
+++ src/TEKRAR/Konu28_ArrayList/C02_ArrayListMetodlari.java
@@ -0,0 +1,21 @@
+package TEKRAR.Konu28_ArrayList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class C02_ArrayListMetodlari {
+    public static void main(String[] args) {
+
+        List<Integer> sayilar=new ArrayList<>(Arrays.asList(5,7,2,9,1,2,5,9,3));
+
+
+
+
+
+
+
+
+
+    }
+}
