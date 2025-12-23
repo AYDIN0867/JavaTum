08385c2ac35cbc6ca343ff4fbd619cc1a743c1aa
ramazan <coolstoves@gmail.com>
2025-12-23 22:56:15 +0300

    23122025

--- src/TEKRAR/Konu28_ArrayList/C05_Remove.java
+++ src/TEKRAR/Konu28_ArrayList/C05_Remove.java
@@ -21,6 +21,27 @@ public class C05_Remove {
         isimler.remove(2);
         System.out.println(isimler);//[Veli, Cemil, Fatma, Hatice]
 
+        //sayilar listesinden 1'i silin
+        System.out.println(sayilar);//[4, 6, 1, 9, 3]
+
+        sayilar.remove(1);
+        System.out.println(sayilar);//[4, 1, 9, 3]
+
+        //sayilar listesinden 9 u sil
+
+        //sayilar.remove(9);
+        /*
+
+        eger list sayılardan oluşuyorsa java girilend eğeri direk indek alı
+        eger sayı olan 4 u silmek istersek  bunu once obje oluşturmak gerekir ki nonprimitive olur
+         */
+
+
+        Integer silinecekSayi=9;
+        sayilar.remove(silinecekSayi);
+        System.out.println(sayilar); //[4, 1, 3]
+
+
 
     }
 }
---
+++ src/TEKRAR/Konu28_ArrayList/C06_SonucVsReturn.java
@@ -0,0 +1,24 @@
+package TEKRAR.Konu28_ArrayList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class C06_SonucVsReturn {
+    public static void main(String[] args) {
+
+
+        List<Integer> sayilar=new ArrayList<>(Arrays.asList(4,6,1,9,3));
+        List<String> isimler=new ArrayList<>(Arrays.asList("Ali","Veli","Cemil","Ayşe","Fatma","Hatice"));
+
+
+        System.out.println(sayilar);//[4, 6, 1, 9, 3]
+
+        //sayilar listesinden 2. indeksdeki elementi 5 yapın...
+
+
+
+
+
+    }
+}
