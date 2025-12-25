b69693265ef8ae76cd14764d3c99fe6acd93f748
ramazan <coolstoves@gmail.com>
2025-12-25 18:36:08 +0300

    25122025

---
+++ src/TEKRAR/Konu28_ArrayList/C07_ArraydenTekrarEdenSayilariSilme.java
@@ -0,0 +1,34 @@
+package TEKRAR.Konu28_ArrayList;
+
+public class C07_ArraydenTekrarEdenSayilariSilme {
+    public static void main(String[] args) {
+
+    //Verilen bir array de tekrar eden sayi varsa
+    //tekrar eden sailari silip,
+    // array i her sayidan sadece 1 tane bulacak şekilde kaydedin
+    // ornek input :{2,3,4,5,2,4,2,2,4,2,4,3,5,1}
+    // output:{1,2,3,4,5}
+
+    int [] arr= {2,3,4,5,2,4,2,2,4,2,4,3,5,1};
+
+        int bulunmaAdedi=1;
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i]==arr[i]){
+
+                Integer silinecekRakam=arr[i];
+
+            }
+
+
+
+
+        }
+
+
+
+
+
+
+    }
+}
