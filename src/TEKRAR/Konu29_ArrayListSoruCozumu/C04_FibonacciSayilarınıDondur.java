04f1910a7300a3c7f15a9222dbe0bbeb6a882353
ramazan <coolstoves@gmail.com>
2025-12-30 23:23:25 +0300

    30122025

--- src/TEKRAR/Konu29_ArrayListSoruCozumu/C04_FibonacciSayilarınıDondur.java
+++ src/TEKRAR/Konu29_ArrayListSoruCozumu/C04_FibonacciSayilarınıDondur.java
@@ -14,6 +14,9 @@ public class C04_FibonacciSayilarınıDondur {
 // ilk n tane fibonacci sayısını bir liste olarak yazıdırn
 // 0 1 1 2 3 5 8 13 21 34 45
 
+        System.out.println(nTaneSeriOlustur(4));//[0, 1, 1, 2]
+        System.out.println(nTaneSeriOlustur(10));//[0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+
 
         Scanner scanner=new Scanner(System.in);
         System.out.println("Lütfen kaçtane fibonacci sayısı istediğinizi griniz");
@@ -28,7 +31,7 @@ public class C04_FibonacciSayilarınıDondur {
 
         if (n<=0){
 
-            System.out.println("sıfırdan kucuk sayı girilemez");
+            System.out.println("pozitif bir sayı giriniz....");
 
         } else if (n==1) {
 
@@ -55,4 +58,41 @@ public class C04_FibonacciSayilarınıDondur {
         System.out.println(fibonacciSerisi);
 
     }
+
+    //verilen pozitif bir n tamsayisini alarak,
+    // bize ilk n tane fibonacci sayısını list olarak donduren bir metod olusturun...
+
+    public static List<Integer>nTaneSeriOlustur(int seridekiElementSayisi){
+        List<Integer> fibonacciSerisi=new ArrayList<>();
+        if (seridekiElementSayisi<=0){
+
+            System.out.println("sıfırdan kucuk sayı girilemez");
+
+        } else if (seridekiElementSayisi==1) {
+
+            fibonacciSerisi.add(0);
+
+        } else if (seridekiElementSayisi==2) {
+
+            fibonacciSerisi.add(0);
+            fibonacciSerisi.add(1);
+
+        }else {
+
+            fibonacciSerisi.add(0);
+            fibonacciSerisi.add(1);
+
+            for (int i = 2; i <seridekiElementSayisi ; i++) {
+
+                fibonacciSerisi.add(fibonacciSerisi.get(i-2)+fibonacciSerisi.get(i-1));
+
+            }
+
+        }
+
+        return fibonacciSerisi;
+    }
+
+
+
 }
---
+++ src/TEKRAR/Konu29_ArrayListSoruCozumu/VerilenArrayiListeCevirme.java
@@ -0,0 +1,14 @@
+package TEKRAR.Konu29_ArrayListSoruCozumu;
+
+public class VerilenArrayiListeCevirme {
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+
+    }
+}
