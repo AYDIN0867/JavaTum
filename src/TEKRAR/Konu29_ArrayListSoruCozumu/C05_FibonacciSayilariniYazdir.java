2e3b50fb8572ec6d49671661107be6a3c8b6d39f
ramazan <coolstoves@gmail.com>
2025-12-31 18:08:31 +0300

    31122025

---
+++ src/TEKRAR/Konu29_ArrayListSoruCozumu/C05_FibonacciSayilariniYazdir.java
@@ -0,0 +1,13 @@
+package TEKRAR.Konu29_ArrayListSoruCozumu;
+
+public class C05_FibonacciSayilariniYazdir {
+
+    public static void main(String[] args) {
+
+
+
+
+
+
+    }
+}
---
+++ src/TEKRAR/Konu29_ArrayListSoruCozumu/C06_VerilenArrayiListeCevirme.java
@@ -0,0 +1,22 @@
+package TEKRAR.Konu29_ArrayListSoruCozumu;
+
+import java.util.Scanner;
+
+public class C06_VerilenArrayiListeCevirme {
+    public static void main(String[] args) {
+
+//kullanıcıdaan pozitif bir tamsayı alıp,
+// o tamsayıdan kucuk Fibonacci sayılarını bir liste olarak yazdırın...
+//
+
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Lütfen kaçtane fibonacci sayılarını yazdırmak icin ust sayı sinirini girin...");
+        int maxSayi =scanner.nextInt();
+
+
+
+
+
+
+    }
+}
