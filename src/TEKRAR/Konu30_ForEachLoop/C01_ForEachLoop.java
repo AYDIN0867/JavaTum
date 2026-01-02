7b09cb2f88f54bb761677736338b38c2fd2acc90
ramazan <coolstoves@gmail.com>
2026-01-02 19:12:52 +0300

    02012026

---
+++ src/TEKRAR/Konu30_ForEachLoop/C01_ForEachLoop.java
@@ -0,0 +1,43 @@
+package TEKRAR.Konu30_ForEachLoop;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class C01_ForEachLoop {
+    public static void main(String[] args) {
+
+
+// For each loop (enhanced) gelişmiş bir foor looptur
+// bir array veya Collection daki  TUM ELEMENTLERI index veya sıralama
+// OLMAKSIZIN bize getirir....index KULLANMAZ
+ //indekslerden kaynaklanan hataları azaltır
+ //indeks kullanmadığı için sıralama gereken işlemlerde kullanılmaz...
+
+
+int  [] arr ={3,4,5,2,3,4,5,1,2,3};
+
+        List<Integer> sayilar=new ArrayList<>(Arrays.asList(2,8,5,6,7,4,2,3,4,5,6,4,5,4,3));
+
+  //arr deki elementlerin toplmını yazdırın
+
+  int toplam=0;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            toplam+=arr[i];
+
+
+        }
+
+        System.out.println("for loop ile arr deki elementlerin toplmı"+ toplam);
+
+toplam=0;
+
+
+
+
+
+
+    }
+}
