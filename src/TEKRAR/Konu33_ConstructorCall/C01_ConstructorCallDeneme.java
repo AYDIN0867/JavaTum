f8767c02753f03e1ccf1ea3d3f88c977c7ea1dba
ramazan <coolstoves@gmail.com>
2026-01-14 03:24:37 +0300

    14012026

--- src/TEKRAR/Konu33_ConstructorCall/C01_ConstructorCallDeneme.java
+++ src/TEKRAR/Konu33_ConstructorCall/C01_ConstructorCallDeneme.java
@@ -2,6 +2,18 @@ package TEKRAR.Konu33_ConstructorCall;
 
 public class C01_ConstructorCallDeneme {
 
+    C01_ConstructorCallDeneme(){System.out.println("parametresiz constructor calisti...");}
+
+    C01_ConstructorCallDeneme(int sayi){System.out.println("int parametreli constructor calisti...");}
+
+    C01_ConstructorCallDeneme(String str){
+        this();
+        System.out.println("string parametreli constructor calisti...");
+
+    }
+
+
+
     public static void main(String[] args) {
 
         //obje olustrurlup ilk deger atamasının yapılması icin MUTLAKA
@@ -18,19 +30,16 @@ public class C01_ConstructorCallDeneme {
         Metod isimleri genel kural: kucuk harfle baslar ve class ismi ile ayni olmaz...
 ancak class ismi ile ayni olsa da CTE vermez...
          */
-
         C01_ConstructorCallDeneme obj=new C01_ConstructorCallDeneme();//parametresiz constructor calisti...
         obj.C01_ConstructorCallDeneme(); //parametresiz metod calisti...
 
 
+        C01_ConstructorCallDeneme obj3=new C01_ConstructorCallDeneme("selim");
+
 
     }
 
-    C01_ConstructorCallDeneme(){System.out.println("parametresiz constructor calisti...");}
 
-    C01_ConstructorCallDeneme(int sayi){System.out.println("int parametreli constructor calisti...");}
-
-    C01_ConstructorCallDeneme(String str){System.out.println("string parametreli constructor calisti...");}
 
 
     void C01_ConstructorCallDeneme(){System.out.println("parametresiz metod calisti...");
