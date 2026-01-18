2bb5208b004c84b0e507dd3313f200199609d34f
ramazan <coolstoves@gmail.com>
2026-01-18 17:01:07 +0300

    18012026

--- src/TEKRAR/Konu33_ConstructorCall/C05_ConstructorCall.java
+++ src/TEKRAR/Konu33_ConstructorCall/C05_ConstructorCall.java
@@ -21,11 +21,22 @@ public  C05_ConstructorCall(String isim){
 
 public C05_ConstructorCall(String isim, int yas ){
 
+    this("Murat");
     this.yas=45;
 
 
 }
 
+    public static void main(String[] args) {
+
+    C05_ConstructorCall obj1=new C05_ConstructorCall("Esra");
+    C05_ConstructorCall obj2=new C05_ConstructorCall("Fatih",35);
+
+
+
+
+
+    }
 
 
 }
