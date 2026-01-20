e6e8e5eec5fcfcdf9d064355e3d82954474bcff9
ramazan <coolstoves@gmail.com>
2026-01-20 13:51:34 +0300

    20012026

---
+++ src/TEKRAR/StaticKeyWord/C02_HastaneStaticKeyWord.java
@@ -0,0 +1,37 @@
+package TEKRAR.StaticKeyWord;
+
+public class C02_HastaneStaticKeyWord {
+
+    static String hastaneIsmi="Yıldiz Hastanesi";
+    static String hastaneAdresi="Cankaya";
+    static String bashekim="Dr Esra";
+
+    String perIsim="isim atanmamis";
+    String perAdres="adres atanmamis";
+    String perTel="tel atanmamis";
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @Override
+    public String toString() {
+        return "Instance Özellikler :" +
+                "perIsim='" + perIsim +
+                " --- perAdres='" + perAdres +
+                " ---perTel='" + perTel +
+                "\nStatic ozellikler : "+
+                "hastaneIsmi : "+ hastaneIsmi+
+                " --- hastaneAdresi : "+ hastaneAdresi+
+                " --- bashekim : "+bashekim;
+
+    }
+}
---
+++ src/TEKRAR/StaticKeyWord/C05_StaticVariablesHatalıHocadan.java
@@ -0,0 +1,55 @@
+package TEKRAR.StaticKeyWord;
+
+public class C05_StaticVariablesHatalıHocadan {
+
+    static int sayi=20;
+    int yas=40;
+
+
+    public C05_StaticVariablesHatalıHocadan(){
+
+        sayi++;
+        yas++;
+    }
+
+    public static int metod1 (){
+
+       return sayi;
+    }
+
+    public int metod2(){
+
+        return yas;
+    }
+
+    public static void main(String[] args) {
+
+        C05_StaticVariablesHatalıHocadan obj1=new C05_StaticVariablesHatalıHocadan();
+        C05_StaticVariablesHatalıHocadan obj2=new C05_StaticVariablesHatalıHocadan();
+        C05_StaticVariablesHatalıHocadan obj3=new C05_StaticVariablesHatalıHocadan();
+
+
+
+        System.out.println(obj3.sayi+","+ obj3.yas);
+        System.out.println(obj1.sayi+","+ obj1.yas);
+
+/*
+
+int sayi = 20;
+
+System.out.println(sayi++); // 20 yazdırır, sonra 21 olur
+System.out.println(++sayi); // önce 22 olur, 22 yazdırır
+23,41
+23,41
+
+
+ */
+
+
+
+
+    }
+
+
+
+}
