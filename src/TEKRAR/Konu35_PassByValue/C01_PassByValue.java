c9792e051897b20b37da72588e37d7af39100293
ramazan <coolstoves@gmail.com>
2026-01-25 17:30:20 +0300

    25012026

--- src/TEKRAR/Konu35_PassByValue/C01_PassByValue.java
+++ src/TEKRAR/Konu35_PassByValue/C01_PassByValue.java
@@ -1,14 +1,14 @@
 package TEKRAR.Konu35_PassByValue;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class C01_PassByValue {
     public static void main(String[] args) {
 
-       String str="Hayır gör hayır olsun..";
-
+        String str="Hayır gör hayır olsun..";
         System.out.println(str.toUpperCase());//HAYIR GÖR HAYIR OLSUN..
-
         System.out.println(str);//Hayır gör hayır olsun..
 
 
@@ -21,17 +21,36 @@ public class C01_PassByValue {
 //        Scanner scanner=new Scanner(System.in);
 //        String  girilenCumle=scanner.nextLine();
 
+        isimDüzenleYazdir("pelin su yılmaz denir");
+
     }
 
     public static void buyukHarfCevir(String girilenCumle){
-
-
-       girilenCumle=girilenCumle.substring(0,1).toUpperCase()+girilenCumle.substring(1).toLowerCase();
-       girilenCumle=girilenCumle;
-
+        girilenCumle=girilenCumle.toUpperCase();
         System.out.println(girilenCumle);
 
     }
 
+    public static void isimDüzenleYazdir(String str){
+
+        List<String>isimParcali= Arrays.asList(str.split(" "));
+        String yeniIsim="";
+
+        for (int i = 0; i < isimParcali.size(); i++) {
+
+            yeniIsim+=isimParcali.get(i).substring(0,1).toUpperCase()+
+                    isimParcali.get(i).substring(1).toLowerCase();
+
+            if (i<isimParcali.size()-1){
+                yeniIsim+=" ";
+            }
+
+
+        }
+
+        System.out.println("ismin düzenlenmiş hali : "+ yeniIsim);
+
+
+    }
 
 }
