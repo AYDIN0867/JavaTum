2f3008c60a754df7439cf6b8657d3c839bb74bae
ramazan <coolstoves@gmail.com>
2026-01-29 23:51:25 +0300

    29012026

--- src/TEKRAR/Konu35_PassByValue/C03_DegisikligiKaliciYapma.java
+++ src/TEKRAR/Konu35_PassByValue/C03_DegisikligiKaliciYapma.java
@@ -9,32 +9,29 @@ public class C03_DegisikligiKaliciYapma {
 // kucuk yapıp sonunda. yoksa nokta koyup bize donduren bi metod olusturun
 
 
-
-
 //kullanıcıdan bir cumle alıp
 // metod ile duzenleyin ve yeni halini kaydedin
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("lütfen bir cümle giriniz....");
-        String cumle=scanner.nextLine();
-        cumle=cumleyiDuzenle(cumle);
-        System.out.println("main metodda cumlenin yeni hali "+ cumle);
-
-
+        String cumle = scanner.nextLine();
+        cumle = cumleyiDuzenle(cumle);
+        System.out.println("main metodda cumlenin yeni hali " + cumle);
 
 
     }
 
 
-    public static String cumleyiDuzenle(String cumle){
+    public static String cumleyiDuzenle(String cumle) {
 
-        cumle=cumle.substring(0,1).toUpperCase()+
+        cumle = cumle.substring(0, 1).toUpperCase() +
                 cumle.substring(1).toLowerCase();
 
-        if (cumle.charAt(cumle.length()-1)!='.'){
-            cumle+=".";
+        if (cumle.charAt(cumle.length() - 1) != '.') {
+            cumle += ".";
 
         }
 
+        return cumle;
 
     }
 
---
+++ src/TEKRAR/Konu35_PassByValue/C04_IndirimliFiyatYazdirma.java
@@ -0,0 +1,12 @@
+package TEKRAR.Konu35_PassByValue;
+
+public class C04_IndirimliFiyatYazdirma {
+
+    public static void main(String[] args) {
+
+        //3 farklı metod oluşturun
+        //verilen fiyat variablesine %10,%20,%40 indirim yapın...
+
+
+    }
+}
