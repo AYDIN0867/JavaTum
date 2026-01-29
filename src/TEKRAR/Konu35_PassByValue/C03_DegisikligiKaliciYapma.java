0e890fb7fe5c5738f8a78e695446a1969e864826
ramazan <coolstoves@gmail.com>
2026-01-29 23:34:05 +0300

    29012026

--- src/TEKRAR/Konu35_PassByValue/C03_DegisikligiKaliciYapma.java
+++ src/TEKRAR/Konu35_PassByValue/C03_DegisikligiKaliciYapma.java
@@ -1,12 +1,42 @@
 package TEKRAR.Konu35_PassByValue;
 
+import java.util.Scanner;
+
 public class C03_DegisikligiKaliciYapma {
     public static void main(String[] args) {
 
+//verilen bir cümlede iki harf buyuk geriye kalan harfleri
+// kucuk yapıp sonunda. yoksa nokta koyup bize donduren bi metod olusturun
 
+
+
+
+//kullanıcıdan bir cumle alıp
+// metod ile duzenleyin ve yeni halini kaydedin
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("lütfen bir cümle giriniz....");
+        String cumle=scanner.nextLine();
+        cumle=cumleyiDuzenle(cumle);
+        System.out.println("main metodda cumlenin yeni hali "+ cumle);
 
 
 
 
     }
+
+
+    public static String cumleyiDuzenle(String cumle){
+
+        cumle=cumle.substring(0,1).toUpperCase()+
+                cumle.substring(1).toLowerCase();
+
+        if (cumle.charAt(cumle.length()-1)!='.'){
+            cumle+=".";
+
+        }
+
+
+    }
+
+
 }
