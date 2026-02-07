c696dd4c478359f0ec6e27c406c3580a898765c1
ramazan <coolstoves@gmail.com>
2026-02-07 21:26:12 +0300

    07022026

---
+++ src/TEKRAR/Konu36_ImmuTableClass/C01_ImmutableClass.java
@@ -0,0 +1,35 @@
+package TEKRAR.Konu36_ImmuTableClass;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class C01_ImmutableClass {
+
+    public static void main(String[] args) {
+
+
+        String str="Az ye Az konus";
+        //immutable class larda metodlarla yapılan değişiklik KALICI olmaz
+
+        str.toUpperCase();
+        System.out.println(str.substring(2)); //ye Az konus
+
+        System.out.println(str);//Az ye Az konus
+
+        List<String> isimler=new ArrayList<>(Arrays.asList("Cansu","Asuman","Hamza"));
+        //mutable Classlarda metod ile yapılan değğişiklikm kalıcı olur
+        System.out.println(isimler);//[Cansu, Asuman, Hamza]
+        isimler.add("Ali");
+        isimler.remove(0);
+        System.out.println(isimler);//[Asuman, Hamza, Ali]
+
+
+
+
+
+    }
+
+
+
+}
