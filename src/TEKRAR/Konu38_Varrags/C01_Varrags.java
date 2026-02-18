ad5e67d0e56052431fae4cb7bf217ec474af3eba
ramazan <coolstoves@gmail.com>
2026-02-18 23:59:22 +0300

    2359

--- src/TEKRAR/Konu38_Varrags/C01_Varrags.java
+++ src/TEKRAR/Konu38_Varrags/C01_Varrags.java
@@ -5,10 +5,17 @@ public class C01_Varrags {
 
 topla(3,5);//8
 topla(3,4,5);//12
+        topla(1,2,3,4,5,6);
 
 
 
     }
+public static void  topla(int... a){
+       //buna varrags diyoruz...
+    //java farklı sayıda parametre yazmak verine
+    //Array altyapısını kullanan Varrag kullanır...
+
+}
 
     public static void  topla(int a, int b){
 
