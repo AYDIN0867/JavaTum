7f678c377099f9110d1a1fc0646cf75de94b6806
ramazan <coolstoves@gmail.com>
2026-02-20 12:45:21 +0300

    20022026

--- src/TEKRAR/Konu38_Varrags/C01_Varrags.java
+++ src/TEKRAR/Konu38_Varrags/C01_Varrags.java
@@ -10,10 +10,19 @@ topla(3,4,5);//12
 
 
     }
-public static void  topla(int... a){
+public static void  topla(int... sayılar){
        //buna varrags diyoruz...
     //java farklı sayıda parametre yazmak verine
     //Array altyapısını kullanan Varrag kullanır...
+//varrag bir array oldugunu için array gibi kullanırız...
+
+    int toplam=0;
+
+    for(int each:sayılar){
+        toplam+=each;
+    }
+
+    System.out.println(sayılar.length);
 
 }
 
---
+++ src/TEKRAR/Konu38_Varrags/C02_VarragsKullanımı.java
@@ -0,0 +1,22 @@
+package TEKRAR.Konu38_Varrags;
+
+public class C02_VarragsKullanımı {
+
+
+    public static void main(String[] args) {
+        //argument olarak girilen sayilardan
+        // ilki haric geriye kalanlari toplayıp
+        // bulunan toplam deger ile ilk sayının carpımını yazdıran
+        // bir metod olusturun
+        // örn. 2,3,4,5 ==>(3+4+5) =24
+
+
+
+    }
+
+    public static void  islem(int ilkSayi, int... geriyeKalan){
+
+
+    }
+}
+
