eb7608e668e62a01ba651579da5e645ef14e171e
ramazan <coolstoves@gmail.com>
2026-02-21 05:22:34 +0300

    21022026

--- src/TEKRAR/Konu38_Varrags/C02_VarragsKullanımı.java
+++ src/TEKRAR/Konu38_Varrags/C02_VarragsKullanımı.java
@@ -10,11 +10,22 @@ public class C02_VarragsKullanımı {
         // bir metod olusturun
         // örn. 2,3,4,5 ==>(3+4+5) =24
 
-
+islem(2,3,4,5,6,7,8,9); //işlem  sonucu :242
 
     }
 
     public static void  islem(int ilkSayi, int... geriyeKalan){
+
+    int toplam=0;
+
+    for(int each: geriyeKalan){
+
+        toplam+=each;
+
+    }
+
+
+        System.out.println("işlem  sonucu :"+ ilkSayi+toplam);
 
 
     }
