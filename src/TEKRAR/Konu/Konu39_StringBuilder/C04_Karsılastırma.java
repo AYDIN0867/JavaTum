f81654eb107784ca12660e7d67763f5fef0fab97
ramazan <coolstoves@gmail.com>
2026-02-24 23:03:40 +0300

    242122026

--- src/TEKRAR/Konu/Konu39_StringBuilder/C04_Karsılastırma.java
+++ src/TEKRAR/Konu/Konu39_StringBuilder/C04_Karsılastırma.java
@@ -9,7 +9,22 @@ public class C04_Karsılastırma {
 
     //aynı metne sahip olan str, sb ve sb1 i karşılaştıralım...
 
+       // System.out.println(str==sb);//Operator '==' cannot be applied to 'java.lang.String', 'java.lang.StringBuilder'
+/*
+java non-primitive data turlerinın farklı data turelrinin  ==ile   karşılaştırılamsına izin vermez
+StringBuilderler metin aynı olsa da eşit eşitte false doner
+ */
 
+//2- equals () metodu ile karşılaştırma...
+
+        System.out.println(str.equals(sb));//false
+        //Result of 'str.equals(sb)' is always 'false'
+        //'equals' between objects of inconvertible types 'String' and 'StringBuilder'
+
+        System.out.println(sb.equals(sb1));//false
+//Suspicious call to 'equals()' on 'StringBuilder' object
+
+        System.out.println(sb.equals(sb));//true sadece kendisi ile karşılaştırmada ture diğerlerinde false verir
 
 
     }
