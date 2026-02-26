629fb1299e29a55bd76bb0b373c2cc09e081ceb1
ramazan <coolstoves@gmail.com>
2026-02-26 23:07:21 +0300

    26022026

---
+++ src/TEKRAR/Konu/Konu40_AccessModifier/package1/AynıPackageFarklıClass.java
@@ -0,0 +1,14 @@
+package TEKRAR.Konu.Konu40_AccessModifier.package1;
+
+public class AynıPackageFarklıClass {
+
+static private int sayPrivate;
+static String strDefaultAccMod;
+static protected char chrProtected;
+static public double dblPublic;
+
+
+
+
+
+}
