4e11a02bbeda20f327c5a0832887ce9ae1139499
ramazan <coolstoves@gmail.com>
2026-02-27 22:20:44 +0300

    27022026

---
+++ src/TEKRAR/Konu/Konu41_Encapsulation/C01.java
@@ -0,0 +1,15 @@
+package TEKRAR.Konu.Konu41_Encapsulation;
+
+public class C01 {
+
+    public String strPub="Yasasin Public";
+    protected String strPro="Yasasin aile";
+     String strDef="Yasasin package";
+     private  String strPri="Yasasin package";
+
+     public  static String strPubs="Yasasin public static";
+
+
+
+
+}
