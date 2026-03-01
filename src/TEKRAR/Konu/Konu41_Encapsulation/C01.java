e1a2bb9ed8d399f40be1ad34d00e8bbef6922d83
ramazan <coolstoves@gmail.com>
2026-03-01 16:42:08 +0300

    01032026

--- src/TEKRAR/Konu/Konu41_Encapsulation/C01.java
+++ src/TEKRAR/Konu/Konu41_Encapsulation/C01.java
@@ -8,6 +8,20 @@ public class C01 {
      private  String strPri="Yasasin package";
 
      public  static String strPubs="Yasasin public static";
+     protected   static String strPros="Yasasin aile static";
+     static String strDefs="Yasasin package static";
+     private static String strPris="Yasasin privacy static";
+
+
+
+
+//read ve write ayırımını acces modifier ile yapamıyoruz...
+// işte burada encapsulation denir az da olsa data hiding de denir
+//erişim okuma yetkisi için GETER
+//yazma yani değer atama yetkisi için SETTER ile yaparız...
+
+
+
 
 
 
---
+++ src/TEKRAR/Konu/Konu41_Encapsulation/C02_Runner.java
@@ -0,0 +1,29 @@
+package TEKRAR.Konu.Konu41_Encapsulation;
+
+public class C02_Runner {
+    public static void main(String[] args) {
+        /*
+
+        Access Modifier kullanarak bir variable'ye
+        ulaşabiliyorsak o variableyi hem kullanabilir
+        hem de varaiableye deger ataması yapabiliriz
+
+        Ulaşamıyorsak da ne kullanabilir
+        ne de deger ataması yapabiliriz...
+
+         */
+
+/*
+Static variablelere ulaşmak için obje
+oluşturmaya gerek olmaz,
+static variablelerin diger adı
+class variablelerdir class isimleri ile kullanılırlar
+
+ */
+
+
+
+
+    }
+
+}
