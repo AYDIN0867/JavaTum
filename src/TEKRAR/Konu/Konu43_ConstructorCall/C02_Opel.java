b1a88cdf2cb9a57d2d283da34ed9c1c2bd91f481
ramazan <coolstoves@gmail.com>
2026-03-03 22:07:13 +0300

    0303

--- src/TEKRAR/Konu/Konu43_ConstructorCall/C02_Opel.java
+++ src/TEKRAR/Konu/Konu43_ConstructorCall/C02_Opel.java
@@ -2,7 +2,11 @@ package TEKRAR.Konu.Konu43_ConstructorCall;
 
 public class C02_Opel extends C01_Car {
 
-
+protected  String marka="Opel";
+protected  String motor="cevreye duyarlı motor";
+protected  String lastik="Pirelli";
+protected  String guvenlik="DGS1001";
+protected  String FREN="ABS";
 
 
 
--- src/TEKRAR/Konu/Konu43_ConstructorCall/C03_Corsa.java
+++ src/TEKRAR/Konu/Konu43_ConstructorCall/C03_Corsa.java
@@ -3,7 +3,31 @@ package TEKRAR.Konu.Konu43_ConstructorCall;
 public class C03_Corsa extends C02_Opel{
 
     String model="Corsa";
+    String kasa="Corsa kasasi";
+    String motor="vvdi";
+    String lastik="15 inc Pirelli";
 
+    String uretimYeri="Almanya";
+    String aku="volta aku";
+
+    public static void main(String[] args) {
+
+        C03_Corsa corsa1=new C03_Corsa();
+        System.out.println(corsa1.aku);//volta aku
+        System.out.println(corsa1.model);//Co
+
+
+        // child class dan olusuturulan bir obje
+        // sadece kendi class'ındaki ozelliklere değil
+        // parent classlardaki özelliklere de sahip olurlar
+
+        System.out.println(corsa1.marka);//Opel
+        System.out.println(corsa1.guvenlik);//dgs1001
+
+
+
+
+    }
 
 
 
