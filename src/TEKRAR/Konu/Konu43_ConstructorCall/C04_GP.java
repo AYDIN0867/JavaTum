c78211e2b9c5e78af57ce282045ea8ed44776b4f
ramazan <coolstoves@gmail.com>
2026-03-09 23:33:15 +0300

    09032026

--- src/TEKRAR/Konu/Konu43_ConstructorCall/C04_GP.java
+++ src/TEKRAR/Konu/Konu43_ConstructorCall/C04_GP.java
@@ -8,6 +8,17 @@ public class C04_GP {
 
     }
 
+    C04_GP(String s){
+
+        this(a:4);
+        System.out.println("gp parametreli constructore calisti");
+    }
+
+    C04_GP(int a){
+
+        System.out.println("GP int parametreli constructor calisti");
+
+    }
 
 
 }
