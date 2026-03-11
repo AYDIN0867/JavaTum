b37da1f40f950211be563fb737d311074a3cf21f
ramazan <coolstoves@gmail.com>
2026-03-11 23:49:30 +0300

    12

--- src/TEKRAR/Konu/Konu43_ConstructorCall/C06_Child.java
+++ src/TEKRAR/Konu/Konu43_ConstructorCall/C06_Child.java
@@ -26,6 +26,16 @@ public class C06_Child extends C05_P {
     public static void main(String[] args) {
 
         C06_Child child1=new C06_Child(10);
+        //super(); default olarak parametresiz
+//main metoddan başlar
+// int 10 parametresini alacak constructor call var mı bakılır
+// ki var C06_Child (int a) ya gider
+//obje oluşturmadan extend avr mı diye sorar
+// ki burda class extend kullandığından ilk satırda mutlaka constructor
+//call var mı bakar hiç bi şey yoksa default olarak super() vardır
+//bu super de önce  parent class daki parametersiz constructor çalışssın demektir
+//parent class da parametersiz class var adı da C05_(){
+// "P parametresiz class çalıştı"}
 
 
     }
