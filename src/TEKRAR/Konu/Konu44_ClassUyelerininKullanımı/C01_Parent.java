56e50db7f5572283cb73d3bbf10de47ee82e7ceb
ramazan <coolstoves@gmail.com>
2026-03-13 03:09:22 +0300

    1303

---
+++ src/TEKRAR/Konu/Konu44_ClassUyelerininKullanımı/C01_Parent.java
@@ -0,0 +1,12 @@
+package TEKRAR.Konu.Konu44_ClassUyelerininKullanımı;
+
+public class C01_Parent {
+
+    String str="Calismak isitir...";
+    String s="Anıl";
+
+
+    public void method2(){System.out.println("Parent method2");}
+
+    public void method3() {System.out.println("Parent method3");}
+}
