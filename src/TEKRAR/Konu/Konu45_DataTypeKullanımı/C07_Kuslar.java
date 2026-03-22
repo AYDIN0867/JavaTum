992f2a4d2e3adbd497700a28838614d2d8e01696
ramazan <coolstoves@gmail.com>
2026-03-22 23:39:38 +0300

    22022025

--- src/TEKRAR/Konu/Konu45_DataTypeKullanımı/C07_Kuslar.java
+++ src/TEKRAR/Konu/Konu45_DataTypeKullanımı/C07_Kuslar.java
@@ -4,6 +4,7 @@ public class C07_Kuslar extends C06_HayvanlarMetod {
 
     public  void kanat(){System.out.println("kanatlidirlar");}
     public  void solunum(){System.out.println("akciger");}
+    public  void beslenme(){System.out.println("beslenirler");}
     public  void gaga(){System.out.println("gagalıdırlar");}
     public  void cogalma(){System.out.println("yumurta ile cogalırlar");}
 
---
+++ src/TEKRAR/Konu/Konu45_DataTypeKullanımı/C08_AvcıKuslar.java
@@ -0,0 +1,47 @@
+package TEKRAR.Konu.Konu45_DataTypeKullanımı;
+
+public class C08_AvcıKuslar extends C07_Kuslar {
+
+    public void hareket(){System.out.println("ucarlar");}
+
+    public void beslenme (){System.out.println("ucarlar");}
+
+    public void pence (){System.out.println("pencelidirler");}
+
+    public void gaga  (){System.out.println("sivri gagalı");}
+
+    public static void main(String[] args) {
+
+
+
+        /*
+
+        eger özellikler METOD olarak oluşturlmuş ise
+        yine aramaya data Turu olan Class'dan baslarız
+
+        ANCAK bulduktan sonra,
+        Constructor oldugu Class'a kadar,
+        DAHA GUNCE  bilgi var mı diye kontrol ede3riz...
+
+
+        data class ından aramaya basladıgından
+        özelliği bulamazsa CTE verir zaten daha günceli olup olmadıgını
+        metod un yanınna gelen yukarı aşağı oklar gösterir
+
+
+
+
+
+
+
+         */
+
+
+
+    }
+
+
+
+
+
+}
