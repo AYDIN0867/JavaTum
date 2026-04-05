51deb5aeb75a128fa7265a65c6285e0b6cf910a5
ramazan <coolstoves@gmail.com>
2026-04-05 20:28:53 +0300

    0504

--- src/TEKRAR/Konu/Konu46_Overriding/C04_Parent.java
+++ src/TEKRAR/Konu/Konu46_Overriding/C04_Parent.java
@@ -3,8 +3,25 @@ package TEKRAR.Konu.Konu46_Overriding;
 public class C04_Parent {
 
 
-
+public void metod1(){
 
 
 
 }
+
+
+
+
+public void solunum(){
+
+
+}
+
+public void method1(int sayi){
+
+
+}
+
+}
+
+
--- src/TEKRAR/Konu/Konu46_Overriding/C05_Child.java
+++ src/TEKRAR/Konu/Konu46_Overriding/C05_Child.java
@@ -1,4 +1,35 @@
 package TEKRAR.Konu.Konu46_Overriding;
 
-public class C05_Child {
+public class C05_Child extends C04_Parent {
+
+   /*
+
+   Child class'ta parent class'tan bir metodu
+   override etmek istersek öncelikli olarak parent class
+   ile aynı SIGNUTARE  sahip bir metod olusturulabilir
+   signature e sahip bir metod olusturulabilir signature
+   farklı olursa ismin aynı olması override sağlamaz...
+
+    */
+
+public void metod2(){
+
+
+
+
+
+}
+
+
+public void solunum(){
+
+
+}
+
+public void method1(){
+
+
+
+}
+
 }
